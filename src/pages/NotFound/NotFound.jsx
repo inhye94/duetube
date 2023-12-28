e127ec3ddba8c2bbb9130bfbbae280c128832872
DuetoPark <inhye.park.dev@gmail.com>
2023-12-28 21:04:16 +0900

    💄 NotFount 페이지 구현

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import styles from "./NotFound.module.css";
 
 export default function NotFound() {
   return (
-    <section>
-      <h1>악 페이지 없서용</h1>
+    <section className={styles.error}>
+      <div className={styles.box}>
+        <p>
+          앗 당신은 길을 잃었어요~!
+          <br />
+          주머니 속의 돈을 저에게 주세요
+        </p>
+
+        <div className={styles["image-group"]}>
+          <img src="/assets/images/img-money.jpeg" alt="give me money" />
+          <p>많이 주세요</p>
+        </div>
+      </div>
     </section>
   );
 }
