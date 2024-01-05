ce02a481ff4cc0f4b4e30e7f784c4b3bae1b999f
DuetoPark <inhye.park.dev@gmail.com>
2024-01-05 12:53:15 +0900

    🔥 테스트 코드 삭제

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,9 @@
 import { createContext, useContext } from "react";
 import Youtube from "../apis/Youtube";
 import YoutubeClient from "../apis/youtubeClient";
-import QaYoutubeClient from "../apis/qaYoutubeClient";
 
 export const YoutubeContext = createContext();
 
-const qaClient = new QaYoutubeClient();
 const client = new YoutubeClient();
 
 const youtube = new Youtube(client);
