368b97a7c1139ecd5ba28fc6b5311f86214c1697
DuetoPark <inhye.park.dev@gmail.com>
2024-01-05 13:56:25 +0900

    🐛 데이터가 없는 경우 때문에, useQueries에서 useQuery로 원복

--- src/pages/VideoDetail/VideoDetail.jsx
+++ src/pages/VideoDetail/VideoDetail.jsx
@@ -1,40 +1,47 @@
 import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { useLocation } from "react-router-dom";
+import { useQueries } from "@tanstack/react-query";
+import { useLocation, useParams } from "react-router-dom";
 import { useYoutubeApi } from "../../context/YoutubeApiContext";
 
 import Video from "../../components/VideoDetail/Video";
 import Channel from "../../components/VideoDetail/Channel";
 
 export default function VideoDetail() {
+  const { videoId } = useParams();
   const { youtube } = useYoutubeApi();
-  const {
-    state: { video },
-  } = useLocation();
+  const { state } = useLocation();
 
-  const {
-    isLoading,
-    error,
-    data: channel,
-  } = useQuery({
-    queryKey: ["channel", video],
-    queryFn: async () => youtube.channel(video.snippet.channelId),
-    refetchOnWindowFocus: false,
-    staleTime: 1000 * 60 * 5,
+  console.log(state);
+
+  const [detail, channel] = useQueries({
+    queries: [
+      {
+        queryKey: ["detail"],
+        queryFn: async () => youtube.detail(videoId),
+        refetchOnWindowFocus: false,
+      },
+      {
+        queryKey: ["channel"],
+        queryFn: async () => youtube.channel(state.channelId),
+        refetchOnWindowFocus: false,
+      },
+    ],
   });
 
   return (
     <div className="md:flex">
       <div className="w-full md:w-3/4 md:mr-[24px]">
-        {isLoading && <p className="loading">Loading!!!</p>}
-        {error && <p>에러 났서요!</p>}
-        {channel && <Video detail={video} channel={channel} />}
+        {detail.isLoading && <p className="loading">Loading!!!</p>}
+        {detail.error && <p>에러 났서요!</p>}
+        {detail.data && channel.data && (
+          <Video detail={detail.data} channel={channel.data} />
+        )}
       </div>
 
       <div className="w-full md:w-1/4">
-        {isLoading && <p className="loading">Loading!!!</p>}
-        {error && <p>에러 났서요!</p>}
-        {channel && <Channel channel={channel} />}
+        {channel.isLoading && <p className="loading">Loading!!!</p>}
+        {channel.error && <p>에러 났서요!</p>}
+        {channel.data && <Channel channel={channel.data} />}
       </div>
     </div>
   );
